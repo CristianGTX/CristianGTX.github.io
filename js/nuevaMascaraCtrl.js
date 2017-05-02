d18b28b7457601fbf9647d32da169ea500ba4782
cristian.scalzone <cristian.scalzone@egoargentina.com>
2017-05-02 11:53:08 -0300

    Actualizacion

--- js/nuevaMascaraCtrl.js
+++ js/nuevaMascaraCtrl.js
@@ -9,5 +9,25 @@ app.controller("nuevaMascaraCtrl", function($scope){
   vm.productosUpgrade = true;
   vm.preevaluacion = true;
   vm.persona = true;
+  vm.conveniosEspeciales = true;
+  vm.conveniosUpgrades = true;
+  vm.rangoCapitas = false;
+
+  //Abre opcion de rangos de capitas
+  vm.abreRangoCapitas = function() {
+    vm.abreUpgrade = false;
+    vm.rangoCapitas = true;
+  };
+
+  //Abre opcion de rangos de capitas
+  vm.abreUpgradeInicio = function() {
+    if (vm.rangoCapitas) {
+      vm.abreUpgrade = false;
+      vm.rangoCapitas = false;
+      return;
+    }
+    vm.abreUpgrade = !vm.abreUpgrade;
+    vm.rangoCapitas = false;
+  };
 
 });
